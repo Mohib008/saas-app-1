f57aa81dd7b8bd963c9c4f203a25cbe0afd8c287
Mohib Arsala <mohib.0008@gmail.com>
2023-11-12 19:22:17 -0500

    new file and folders are added

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    future: {
+        webpack5: true,
+    },
+    images: {
+        domains: ["placehold.co"],
+    },
+}
+
+module.exports = nextConfig
